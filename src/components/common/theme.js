0c0a462bc7700961a298d34f8096f77c55eb7a25
Arunas Slepikas <dijitle@gmail.com>
2020-03-29 13:32:24 -0500

    DARK MODE TOGGLE!

---
+++ src/components/common/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#FFFFFF",
+  text: "#000000s",
+  toggleBorder: "#FFF",
+  gradient: "linear-gradient(#39598A, #79D7ED)"
+};
+
+export const darkTheme = {
+  body: "#000000",
+  text: "#FFFFFF",
+  toggleBorder: "#000",
+  gradient: "linear-gradient(#091236, #1E215D)"
+};
